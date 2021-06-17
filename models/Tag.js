cca4fb8403db7a886358dfa8b624d0aae31d3beb
Tiffany Washke <twashke@gmail.com>
2021-06-17 15:03:35 -0700

    updated Tag Model to meet specs from README and added comments

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,23 @@
+// Import Important Parts of Sequelize Library
 const { Model, DataTypes } = require('sequelize');
-
+// Import Database connection 
 const sequelize = require('../config/connection.js');
 
+// Initialize Tag Model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
+// Fields and Rules for Tag Model
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
