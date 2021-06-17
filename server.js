0d053d7e81839f3c72b08493f6d10c8598f5176e
Tiffany Washke <twashke@gmail.com>
2021-06-17 14:37:37 -0700

    updated server and created mysql database, server is connected and working

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+//  Declare Dependencies
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection')
 
+// Declare Variable for Express Function
 const app = express();
+// Declare PORT
 const PORT = process.env.PORT || 3001;
 
+// Data Parsing using Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Turn on all routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`)
+  });
 });
