709d284d93b40b3fbc18d1fc364ddcaf4d1c58d3
Tiffany Washke <twashke@gmail.com>
2021-06-17 14:59:50 -0700

    updated Product Models to specs from README

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,46 @@
-// import important parts of sequelize library
+// Import Important Parts of Sequelize Library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+// Import Database connection 
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product Model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Fields and Rules for Product Model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
