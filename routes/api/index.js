f396ca47565a65cf0d1421e0263b8ba1bf7c59f3
Tiffany Washke <twashke@gmail.com>
2021-06-19 19:09:07 -0700

    updated index.js file with comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,10 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// -----------------------------------------|
+//            Maps Router Paths             | 
+// -----------------------------------------|
+
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
